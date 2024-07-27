78c3c2a9b4872fe4f4303fea0306678dcff6b24f
ragalcha <agalcharamaiya@gmail.com>
2024-07-27 23:18:46 +0530

    final

---
+++ src/models/customer.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const customerSchema = new Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		}
+	},
+	{ timestamps: true }
+);
+
+
+export const Customer =
+	mongoose.model.Customer || mongoose.model("Customer", customerSchema);
