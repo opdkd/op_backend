be14f9d52fbcbbe8adc91ac6c22820860b624564
opdkd <opdhaker48@gmail.com>
2024-07-30 12:32:57 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,7 @@ import {DB_NAME} from "../constants.js";
 
 const dbConection = async () => {
     try {
-        const mongooseConRes = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const mongooseConRes = await mongoose.connect(`mongodb+srv://op:op@cluster0.glt1pup.mongodb.net/${DB_NAME}`);
         console.log("MongoDB connected: ", mongooseConRes.connection.port);
         return "ramaiya agalcha success";
     } catch (error) {
